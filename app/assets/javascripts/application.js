a407ffa027fd85b4b382387f24b7008ebc42ca00
wwlee94 <wwlee94@naver.com>
2019-11-18 15:00:01 +0900

    welcome 컨트롤러 생성 및 첫번째 모델 생성

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 //= require jquery
-//= require bootstrap-sprockets
+//= require bootstrap-sprockets <<<
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
+  # root 라우트 정의는 routes.rb 파일에서 최상위에 위치해야 한다.
+  root 'welcome#index'
+  # get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
