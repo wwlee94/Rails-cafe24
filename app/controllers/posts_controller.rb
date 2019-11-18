db8815d333e11b1ed2ac9b56f5c9f9b6b9837e92
wwlee94 <wwlee94@naver.com>
2019-11-18 16:05:56 +0900

    scaffold 제너레이터 사용 및 posts CRUD 기능 추가하기

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  # 이 기능은 필터라고 하며 이전에는 before_filter, after_filter, around_filter로 사용되었지만
+  # 레일스 4부터 _filter가 _action으로 변경되었다. 따라서 각각 before_action, after_action, around_action으로 사용된다.
 
   # GET /posts
   # GET /posts.json
@@ -61,6 +63,7 @@ class PostsController < ApplicationController
     end
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_post
@@ -68,7 +71,9 @@ class PostsController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
+    # Strong Parameters 기능 -> 접근 가능 한 속성을 정의해 둔 것
+    # White List 지정 하기
     def post_params
-      params.require(:post).permit(:content)
+      params.require(:post).permit(:title, :content)
     end
 end
